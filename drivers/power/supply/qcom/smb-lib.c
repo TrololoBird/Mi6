6f9cd17463dccbbef9c3e79af883a019a119b05e
wloot <wlootlxt123@gmail.com>
2018-08-25 14:56:09 +0800

    Revert "dts: smb-lib: increase charing limit value"
    This reverts commit 2c1307ed7a2dafcb2a6e1ea404c5684f120af99b.

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -2000,8 +2000,8 @@ end:
 
 #ifdef CONFIG_MACH_XIAOMI_MSM8998
 #define MAX_CURRENT_PERCENT		100
-#define HIGH_CURRENT_PERCENT		80
-#define MEDIUM_CURRENT_PERCENT		55
+#define HIGH_CURRENT_PERCENT		70
+#define MEDIUM_CURRENT_PERCENT		50
 #endif
 int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 				const union power_supply_propval *val)
